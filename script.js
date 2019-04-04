1f83bccb8cc64ed22c7a46f513acf71abeb68902
Elysee Elius <elyseeelius@outlook.com>
2019-04-04 00:09:12 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,45 +1,58 @@
 const box = document.getElementsByTagName('td');
 let countClick = 0;
 const goal = [
-  [1,2,3],
-  [4,5,6],
-  [7,8,9]
-  [1,4,7],
-  [2,5,8],
-  [3,6,9],
-  [1,5,9],
-  [3,5,7]
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+    [1, 4, 7],
+    [2, 5, 8],
+    [3, 6, 9],
+    [1, 5, 9],
+    [3, 5, 7]
 ]
 
 let win = [];
+
 for (let i = 0; i < box.length; i++) {
-  box[i].addEventListener('click', function (e) {
-    countClick += 1;
-    if (box[i].innerText == '') {
-      if (countClick % 2 == 0) {
-
-        box[i].innerText = 'o';
-        box[i].style.color = '#000';
-      } else {
-        box[i].innerText = 'x';
-        box[i].style.color = 'red'
-      }
-    }
- 
-    let tagname = (e.target.id);
-    
-    let tagcontent = e.target.innerText;
-    if(!win.includes(tagname)){
-     let intName = parseInt(tagname);
-
-     console.log(intName)
-     
-      // win.push(tagname)
-      // console.log(win)
-    }
-    
- 
+    box[i].addEventListener('click', function (e) {
+        countClick += 1;
+        if (box[i].innerText == '') {
+            if (countClick % 2 == 0) {
+                box[i].innerText = 'o';
+                box[i].style.color = '#000';
+            } else {
+                box[i].innerText = 'x';
+                box[i].style.color = '#ff0000'
+            }
+        }
 
 
-  }, false)
+        let idValue = box[i].id;
+
+        if (!win.includes(idValue)) {
+            let intValue = parseInt(idValue);
+            win.push(intValue)
+            console.log(win)
+        }
+ if(box[i].innerText = 'x' && goal.includes(win)){
+console.log("x has won!");
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }, false)
 }

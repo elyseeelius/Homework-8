5e98dab246f898588b978b4c53c5a8ab2d663591
Elysee Elius <elyseeelius@outlook.com>
2019-04-08 00:18:36 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,6 @@
 const box = document.getElementsByTagName('td');
 let countClick = 0;
-const goal = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-    [1, 4, 7],
-    [2, 5, 8],
-    [3, 6, 9],
-    [1, 5, 9],
-    [3, 5, 7]
-]
 
-let win = [];
 
 for (let i = 0; i < box.length; i++) {
     box[i].addEventListener('click', function (e) {
@@ -25,30 +14,15 @@ for (let i = 0; i < box.length; i++) {
                 box[i].style.color = '#ff0000'
             }
         }
-
-
-        let idValue = box[i].id;
-
-        if (!win.includes(idValue)) {
-            let intValue = parseInt(idValue);
-            win.push(intValue)
-            console.log(win)
+        if (box[0].innerHTML !== "" && box[0].innerHTML === box[1].innerHTML && box[0].innerHTML === box[2].innerHTML) {
+            console.log(" won")
+        } else if (box[3].innerHTML !== "" && box[3].innerHTML === box[4].innerHTML && box[3].innerHTML === box[5].innerHTML) {
+            console.log(' elysee won too')
+        } else if(box[6].innerHTML !== "" && box[6].innerHTML === box[7].innerHTML && box[6].innerHTML === box[8].innerHTML){
+            console.log(' won again')
+        }else if(box[0].innerHTML !== "" && box[0].innerHTML === box[4].innerHTML ){
+            console.log(" 1 4 7")
         }
- if(box[i].innerText = 'x' && goal.includes(win)){
-console.log("x has won!");
- }
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
